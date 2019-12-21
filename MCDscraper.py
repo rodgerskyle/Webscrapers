b90bf088d23aaea7719a567937dfba8b1ef99f01
kyle rodgers <kyle77r@gmail.com>
2019-12-20 22:22:43 -0800

    Working on logic for clicking button to load page

--- MCDscraper.py
+++ MCDscraper.py
@@ -7,6 +7,9 @@ from multiprocessing import Process, Lock
 import pandas as pd
 import os
 
+#Options for chrome
+from selenium.webdriver.chrome.options import Options
+
 #For continually loading new products on page
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
@@ -16,14 +19,21 @@ import time
 
 def scrape(lock, category):
     try:
-        driver = webdriver.Chrome("./chromedriver")
+        #driver = webdriver.Firefox()
+        options = Options()
+        options.add_argument('--no-sandbox')
+        driver = webdriver.Chrome(options=options)
+        #driver = webdriver.Chrome()
         driver.get("https://www.mcdonalds.com/us/en-us/about-our-food/nutrition-calculator.html")
         #Grabs button for each category then will press it
         #button = i.find_element_by_class_name("btn-category-nav")
-        #time.sleep(2)
-    #    driver.execute_script("arguments[0].click();", button)
-        driver.execute_script("arguments[0].click();", category)
-        time.sleep(2)
+        time.sleep(6)
+
+        button = driver.find_element_by_class_name('btn-category-nav')
+#        button = category
+        driver.execute_script("arguments[0].click();", button)
+        #driver.execute_script("arguments[0].click();", category)
+        #time.sleep(6)
         #Parses the page to just text
         content = driver.page_source
         soup = BeautifulSoup(content, "html.parser")
@@ -32,11 +42,14 @@ def scrape(lock, category):
         for names, calories in zip(itemNames, itemCalories):
             #Finds the items' calories
             name=names.text.strip()
+            #strips name string of bad characters
             name=name.replace('®', '')
             name=name.replace('™', '')
             calorie=calories.text.strip()
             #Outputs scraped information to the file
             lock.acquire()
+            f = open("output.txt", "a")
+            print(name + ", " + calorie + ",\n")
             f.write(name + ", " + calorie + ",\n")
             lock.release()
         #Find back button and click that
@@ -51,9 +64,14 @@ def scrape(lock, category):
 
 PATIENCE_TIME = 60
 
-os.remove("output.txt")
-f = open("output.txt", "a")
-parentDriver = webdriver.Chrome("./chromedriver")
+#Try to remove old output file 
+try:
+    os.remove("output.txt")
+except Exception as e:
+    print(e)
+#f = open("output.txt", "a")
+#parentDriver = webdriver.Chrome(chrome_options=options)
+parentDriver = webdriver.Chrome()
 
 parentDriver.get("https://www.mcdonalds.com/us/en-us/about-our-food/nutrition-calculator.html")
 
@@ -68,6 +86,7 @@ lock = Lock()
 #Load up n'th number of processes for scraping
 for category in categories:
         Process(target=scrape, args=(lock, category)).start()
+        break
         #Closes processes after done
 
 #for i in categories:
